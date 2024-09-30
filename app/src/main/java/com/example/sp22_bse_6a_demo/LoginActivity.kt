4c7b5de4e54f776de49d8abc56e0cbddfa0b7d86
Ali Usman <ali.usman@tapcheck.com>
2024-09-30 10:59:23 +0500

    toast and activity transition

--- app/src/main/java/com/example/sp22_bse_6a_demo/LoginActivity.kt
+++ app/src/main/java/com/example/sp22_bse_6a_demo/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sp22_bse_6a_demo
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,13 +16,21 @@ class LoginActivity : AppCompatActivity() {
 
 
     lateinit var binding: ActivityLoginBinding
-    val tag = this@LoginActivity.localClassName
+    val tag = this@LoginActivity.javaClass.name
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
 
         binding.loginBtn.setOnClickListener {
             Log.e(tag, "onCreate: Button clicked -> loginBtn" )
+            Toast.makeText(this@LoginActivity,"Button clicked -> loginBtn",Toast.LENGTH_LONG).show()
+        }
+
+        binding.signupBtn.setOnClickListener {
+            startActivity(
+                Intent(this@LoginActivity, MainActivity::class.java)
+            )
+            finish()
         }
 
     }
